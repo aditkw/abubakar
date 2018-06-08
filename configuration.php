698b081952910f023fea8ea86454c20f3d49c407
aditwalihadi <aditwalihadi14@gmail.com>
2018-06-08 17:14:00 +0700

    fixed

--- configuration.php
+++ configuration.php
@@ -1,13 +1,12 @@
 <?php
 
 class SConfig{
-	var $_site_url 			= 
-"http://lawavedesign.co.id/abubakar-rekan/";
-	var $_site_name 		= "Abu Bakar Rekan";
-	var $_host_name 		= "localhost";
-	var $_database_name 		= "u8113914_4b03b4k4r";
-	var $_database_user 		= "u8113914_4b03b4k4r";
-	var $_database_password		 = "aditkw25";
+	var $_site_url 					= "http://localhost/abubakar/";
+	var $_site_name 				= "Abu Bakar Rekan";
+	var $_host_name 				= "localhost";
+	var $_database_name 		= "db_abubakar";
+	var $_database_user 		= "root";
+	var $_database_password = "aditkw25";
 	var $_table_prefix 			= "lwd_";
 	var $_cms_name 					= "LawaveDesign CMS ver.1.1";
 }
