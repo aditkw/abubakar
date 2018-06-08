7e34000188cab1f83dc45a063421f0dd05044910
aditwalihadi <aditwalihadi14@gmail.com>
2018-06-08 17:21:31 +0700

    fixed untracked

--- application/views/admin/media.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$this->load->view('admin/component/top');
-$this->load->view('admin/component/header');
-$this->load->view('admin/component/left');
-$this->load->view($content);
-$this->load->view('admin/component/footer');
-$this->load->view('admin/component/bottom');
--- application/views/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$this->load->view('component/top');
-$this->load->view('component/header');
-$this->load->view($content);
-$this->load->view('component/footer');
-$this->load->view('component/bottom');
